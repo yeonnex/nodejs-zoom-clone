d6d114eb62f306301030755cdc4b5b4cc8ac3dba
yeonnex <yeonnex@gmail.com>
2021-04-09 23:59:52 +0900

    요청 uuid url 콘솔에 출력

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+// require 는 모듈을 import 하는 것과 비슷한 역할을 한다.
+
 const express = require("express"); //express 도 결국은 모듈이기에, 모듈을 로드한다
 const app = express(); //Creates an Express application
 const server = require("http").Server(app); //http는 node.js의 built-in 모듈
+const {v4:uuidv4} = require('uuid');
+app.set("view engine", "ejs");
 
 // Route
 app.get("/", (req, res) => {
-  res.status(200).send("Hello world");
+  res.redirect(`/${uuidv4()}`);
 });
+app.get("/:room",(req, res)=>{
+    res.render('room', {roomId: req.params.room})
+})
 
 server.listen(3030);
